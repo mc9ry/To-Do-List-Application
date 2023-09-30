81e0d4b7641b2121c01f5b158f17b61a048042a4
mchohan <mchohan@rsimail.com>
2023-09-30 14:36:30 -0400

    changing structure to include both projects in one repository again

---
+++ ToDoClient/client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function Message() {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>This is a test message to verify that React is working.</p>
+        </div>
+    );
+}
+
+export default Message  ;
